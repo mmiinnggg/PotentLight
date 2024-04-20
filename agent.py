89dd6b25ef032701bf3f954cc273042303b6df39
ming <37299829+mmiinnggg@users.noreply.github.com>
2024-04-20 20:19:28 +0800

    Update agent.py

--- agent.py
+++ agent.py
@@ -23,9 +23,9 @@ class DQNAgent(object):
                  ):
         self.env = env
         self.intersection_id = intersection_id
-        self.state_size = state_size
         self.action_size = action_size
         self.batch_size = batch_size #32
+        self.state_size = 8
 
         self.memory_size = 2000
         self.memory = deque(maxlen=self.memory_size)
@@ -38,121 +38,9 @@ class DQNAgent(object):
         self.epsilon = 0.1  # exploration rate
         self.epsilon_min = 0.01
         self.epsilon_decay = 0.995
-        self.learning_rate = 0.005 #0.001
-
-        self.dic_agent_conf = {
-            "LEARNING_RATE": 0.001,
-            "SAMPLE_SIZE": 1000,
-            "BATCH_SIZE": 20,
-            "EPOCHS": 100,
-            "UPDATE_Q_BAR_FREQ": 5,
-            "UPDATE_Q_BAR_EVERY_C_ROUND": False,
-            "GAMMA": 0.8,
-            "MAX_MEMORY_LEN": 10000,
-            "PATIENCE": 10,
-            "D_DENSE": 20,
-            "N_LAYER": 2,
-            "EPSILON": 0.8,
-            "EPSILON_DECAY": 0.95,
-            "MIN_EPSILON": 0.2,
-            "LOSS_FUNCTION": "mean_squared_error",
-            "SEPARATE_MEMORY": False,
-            "NORMAL_FACTOR": 20,
-            "TRAFFIC_FILE": "cross.2phases_rou01_equal_450.xml",
-        }
-        self.dic_traffic_env_conf = {
-            "ACTION_PATTERN": "set",
-            "NUM_INTERSECTIONS": 1,
-            "MIN_ACTION_TIME": 10,
-            "YELLOW_TIME": 5,
-            "ALL_RED_TIME": 0,
-            "NUM_PHASES": 2,
-            "NUM_LANES": 1,
-            "ACTION_DIM": 2,
-            "MEASURE_TIME": 10,
-            "IF_GUI": True,
-            "DEBUG": False,
-
-            "INTERVAL": 1,
-            "THREADNUM": 8,
-            "SAVEREPLAY": True,
-            "RLTRAFFICLIGHT": True,
-
-            "DIC_FEATURE_DIM": dict(
-                D_LANE_QUEUE_LENGTH=(4,),
-                D_LANE_NUM_VEHICLE=(4,),
-
-                D_COMING_VEHICLE=(4,),
-                D_LEAVING_VEHICLE=(4,),
-
-                D_LANE_NUM_VEHICLE_BEEN_STOPPED_THRES1=(4,),
-                D_CUR_PHASE=(1,),
-                D_NEXT_PHASE=(1,),
-                D_TIME_THIS_PHASE=(1,),
-                D_TERMINAL=(1,),
-                D_LANE_SUM_WAITING_TIME=(4,),
-                D_VEHICLE_POSITION_IMG=(4, 60,),
-                D_VEHICLE_SPEED_IMG=(4, 60,),
-                D_VEHICLE_WAITING_TIME_IMG=(4, 60,),
-
-                D_PRESSURE=(1,),
-
-                D_ADJACENCY_MATRIX=(2,)
-            ),
-
-            "LIST_STATE_FEATURE": [
-                "cur_phase",
-                # "time_this_phase",
-                # "vehicle_position_img",
-                # "vehicle_speed_img",
-                # "vehicle_acceleration_img",
-                # "vehicle_waiting_time_img",
-                "lane_num_vehicle",
-                # "lane_num_vehicle_been_stopped_thres01",
-                # "lane_num_vehicle_been_stopped_thres1",
-                # "lane_queue_length",
-                # "lane_num_vehicle_left",
-                # "lane_sum_duration_vehicle_left",
-                # "lane_sum_waiting_time",
-                # "terminal",
-
-                # "coming_vehicle",
-                # "leaving_vehicle",
-                # "pressure",
-
-                # "adjacency_matrix"
-
-            ],
-
-            "DIC_REWARD_INFO": {
-                "flickering": 0,
-                "sum_lane_queue_length": 0,
-                "sum_lane_wait_time": 0,
-                "sum_lane_num_vehicle_left": 0,
-                "sum_duration_vehicle_left": 0,
-                "sum_num_vehicle_been_stopped_thres01": 0,
-                "sum_num_vehicle_been_stopped_thres1": -0.25,
-                "pressure": 0,
-            },
-
-            "LANE_NUM": {
-                "LEFT": 1,
-                "RIGHT": 1,
-                "STRAIGHT": 1
-            },
-
-            "PHASE": [
-                'WSES',
-                'NSSS',
-                'WLEL',
-                'NLSL',
-                # 'WSWL',
-                # 'ESEL',
-                # 'NSNL',
-                # 'SSSL',
-            ],
-
-        }
+        self.learning_rate = 0.001 #0.005
+        self.d_dense = 20
+        self.n_layer = 2
 
         self.model = self._build_model()
         self.target_model = self._build_model()
@@ -165,39 +53,24 @@ class DQNAgent(object):
     def _build_model(self):
         '''Initialize a Q network'''
 
-        dic_input_node = {}
-        for feature_name in self.dic_traffic_env_conf["LIST_STATE_FEATURE"]:
-            if "phase" in feature_name or "adjacency" in feature_name:
-                _shape = self.dic_traffic_env_conf["DIC_FEATURE_DIM"]["D_" + feature_name.upper()]
-            else:
-                _shape = (self.state_size,)
-            dic_input_node[feature_name] = Input(shape=_shape,
-                                                 name="input_" + feature_name)
-
-        dic_flatten_node = {}
-        for feature_name in self.dic_traffic_env_conf["LIST_STATE_FEATURE"]:
-            if len(self.dic_traffic_env_conf["DIC_FEATURE_DIM"]["D_" + feature_name.upper()]) > 1:
-                dic_flatten_node[feature_name] = Flatten()(dic_input_node[feature_name])
-            else:
-                dic_flatten_node[feature_name] = dic_input_node[feature_name]
+        dic_input_node = {"feat1": Input(shape=(1,), name="input_cur_phase"),
+                          "feat2": Input(shape=(8,), name="input_feat2")}
 
         list_all_flatten_feature = []
-        for feature_name in self.dic_traffic_env_conf["LIST_STATE_FEATURE"]:
-            list_all_flatten_feature.append(dic_flatten_node[feature_name])
+        for feature_name in dic_input_node:
+            list_all_flatten_feature.append(dic_input_node[feature_name])
         all_flatten_feature = concatenate(list_all_flatten_feature, axis=1, name="all_flatten_feature")
 
-        locals()["dense_0"] = Dense(self.dic_agent_conf["D_DENSE"], activation="relu", name="dense_0")(
-            all_flatten_feature)
-        for i in range(1, self.dic_agent_conf["N_LAYER"]):
-            locals()["dense_%d" % i] = Dense(self.dic_agent_conf["D_DENSE"], activation="relu", name="dense_%d" % i)(
+        locals()["dense_0"] = Dense(self.d_dense, activation="relu", name="dense_0")(all_flatten_feature)
+        for i in range(1, self.n_layer):
+            locals()["dense_%d" % i] = Dense(self.d_dense, activation="relu", name="dense_%d" % i)(
                 locals()["dense_%d" % (i - 1)])
         q_values = Dense(self.action_size, activation="linear", name="q_values")(
-            locals()["dense_%d" % (self.dic_agent_conf["N_LAYER"] - 1)])
-        network = Model(inputs=[dic_input_node[feature_name]
-                                for feature_name in self.dic_traffic_env_conf["LIST_STATE_FEATURE"]],
+            locals()["dense_%d" % (self.n_layer - 1)])
+        network = Model(inputs=[dic_input_node[feature_name] for feature_name in ["feat1", "feat2"]],
                         outputs=q_values)
-        network.compile(optimizer=RMSprop(lr=self.dic_agent_conf["LEARNING_RATE"]),
-                        loss=self.dic_agent_conf["LOSS_FUNCTION"])
+        network.compile(optimizer=RMSprop(lr=self.learning_rate),
+                        loss="mean_squared_error")
         # network.summary()
         return network
 
