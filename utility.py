8e2f198a079c0ed2bd325bc4e142d6b72b2b82db
ming <37299829+mmiinnggg@users.noreply.github.com>
2024-04-20 16:18:27 +0800

    Update utility.py

--- utility.py
+++ utility.py
@@ -2,11 +2,7 @@ import json
 import argparse
 import numpy as np
 
-def parse_arguments():
-    parser = argparse.ArgumentParser()
-    parser.add_argument("--scenario", type=str, default="hangzhou_1x1_bc-tyc_18041607_1h")
-    parser.add_argument("--num_step", type=int, default=3600)
-    return parser.parse_args()
+
 
 def parse_roadnet(roadnetFile):
     roadnet = json.load(open(roadnetFile))
@@ -107,49 +103,7 @@ def plot_data_lists(data_list,
         plt.show()
 
 
-def sample_aug(state):
-    state_ = {}
-    # state_ = {str(id_): state[str(id_)] for id_ in state}
-    for id_ in state:
-        temp = [e for e in state[str(id_)][0]]
-        tt = temp[3]
-        for i in range(3, 0, -1):
-            temp[i] = temp[i - 1]
-        temp[0] = tt
-        temp[4] = (temp[4] % 4) + 1
 
-        ttemp = np.array(temp)
-        state_size = len(ttemp.flatten())
-        ttemp = np.reshape(ttemp, [1, state_size])
-        state_[str(id_)] = ttemp
-
-    return state_
-
-def sample_aug_single(state):
-
-    temp = [e for e in state[0]]
-    tt = temp[3]
-    for i in range(3, 0, -1):
-        temp[i] = temp[i - 1]
-    temp[0] = tt
-    temp[4] = (temp[4] % 4) + 1
-
-    state_ = np.array(temp)
-    state_size = len(state_.flatten())
-    state_ = np.reshape(state_, [1, state_size])
-
-    return state_
-
-def sample_aug_timing(state):
-
-    temp = [e for e in state[0]]
-    tt = temp[4]
-    for i in range(4, 1, -1):
-        temp[i] = temp[i - 1]
-    temp[1] = tt
-    temp[5] = (temp[5] % 4) + 1
-
-    state_ = np.array(temp)
     state_size = len(state_.flatten())
     state_ = np.reshape(state_, [1, state_size])
 
